fa990f51d67be6ab25ebb3359f91025682c0e1ad
Đỗ Hữu Quang <quangcr7235@gmail.com>
2025-07-02 16:47:15 +0700

    Update TRR3007-3.7 Chu trinh Hamilton.cpp

--- TRR3007-3.7 Chu trinh Hamilton.cpp
+++ TRR3007-3.7 Chu trinh Hamilton.cpp
@@ -4,38 +4,38 @@ int n, u, cnt=0;
 int b[101], vs[101];
 vector<int> a[101];
 void kq(){
-	for(int i=1;i<=n;i++){
-		cout << b[i] << " ";
-	}
-	cout << u << endl;
+    for(int i=1;i<=n;i++){
+	cout << b[i] << " ";
+    }
+    cout << u << endl;
 }
 void hamilton(int i){
-	int t=b[i-1];
-	for(auto j:a[t]){
-		if(i==n+1&&j==u){
-			++cnt;
-			kq();
-		}
-		else if(!vs[j]){
-			vs[j]=1;
-			b[i]=j;
-			hamilton(i+1);
-		    vs[j]=0;
-		}
+    int t=b[i-1];
+    for(auto j:a[t]){
+	if(i==n+1&&j==u){
+	    ++cnt;
+	    kq();
 	}
+	else if(!vs[j]){
+	    vs[j]=1;
+	    b[i]=j;
+	    hamilton(i+1);
+            vs[j]=0;
+	}
+    }
 }
 int main(){
-	freopen("CT.INP","r",stdin);
-	freopen("CT.OUT","w",stdout);
-	cin >> n >> u;
-	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n;j++){
-			int x; cin >> x;
-			if(x) a[i].push_back(j);
-		}
+    freopen("CT.INP","r",stdin);
+    freopen("CT.OUT","w",stdout);
+    cin >> n >> u;
+    for(int i=1;i<=n;i++){
+	for(int j=1;j<=n;j++){
+	    int x; cin >> x;
+	    if(x) a[i].push_back(j);
 	}
-	b[1]=u;
-	vs[u]=1;
-	hamilton(2);
-	cout << cnt << endl;
+    }
+    b[1]=u;
+    vs[u]=1;
+    hamilton(2);
+    cout << cnt << endl;
 }
